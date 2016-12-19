cca802dc9332dd561fcbfce7ac9c6af72f5359bd
Roberto <rangeles@hoopa.io>
2016-12-19 09:35:30 -0600

    My first commit

---
+++ awesomelib/src/main/java/io/hoopa/awesomelib/Awesome.java
@@ -0,0 +1,14 @@
+package io.hoopa.awesomelib;
+
+import android.util.Log;
+
+/**
+ * Created by hoopa on 15/12/16.
+ */
+
+public final class Awesome {
+
+    public static void init(){
+        Log.d("MyLibrary", "Init method and library");
+    }
+}
